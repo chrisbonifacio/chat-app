0524fc2838ef9d60dca48ee80b38aef0c1d822ea
Chris Bonifacio <christopher.bonifacio@gmail.com>
2020-12-21 23:25:43 -0500

    set up initial file structure, express server and web sockets

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+
+http.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
